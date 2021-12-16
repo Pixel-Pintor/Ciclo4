5c3c10ab99c670c0f47e33bb636ea3f1cc72ae4d
Christian Prada <cpradavargas@gmail.com>
2021-12-16 12:55:47 -0500

    Arreglaso Controller

--- Reto_4/src/main/java/com/c4reto4/controller/PeripheralController.java
+++ Reto_4/src/main/java/com/c4reto4/controller/PeripheralController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/supplements")
+@RequestMapping("/api/peripherals")
 @CrossOrigin("*")
 public class PeripheralController {
 
